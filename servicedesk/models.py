9163b19c28f16548812bb9acd21878d0d909bd2d
Gabriel Ferreira <gabriel.ferreira@concretesolutions.com.br>
2016-03-20 23:34:52 -0300

    Starting models

--- servicedesk/models.py
+++ servicedesk/models.py
@@ -3,3 +3,23 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Ticket(models.Model):
+    number = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __unicode__(self):
+        return self.number
+
+class TicketComment(models.Model):
+    ticket = models.ForeignKey(
+        'Ticket',
+        on_delete=models.CASCADE
+    )
+
+class TicketFile(models.Model):
+    ticket = models.ForeignKey(
+        'Ticket',
+        on_delete=models.CASCADE
+    )
